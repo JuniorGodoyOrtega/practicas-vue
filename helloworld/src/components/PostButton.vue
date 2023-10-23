4349954cdf62d5da20c5e4b6b1cdcc1855836dfa
JuniorGodoyOrtega <godoyortegajuniorsilverio@gmail.com>
2023-10-23 20:34:14 -0300

    Mi primer commit

---
+++ helloworld/src/components/PostButton.vue
@@ -0,0 +1,24 @@
+<template>
+<button class="post-btn">Prueba</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.post-btn {
+  background-color: #4CAF50; /* Green */
+  border: 0;
+  outline: 0;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+.post-btn:hover {
+  background-color: #3e8e41;
+  color: white;
+}
+</style>
---
+++ helloworld/src/components/PostCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="post-card">
+        <h1>Hola Mundo</h1>
+        <p>Este es un componente, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum perspiciatis temporibus hic quam necessitatibus impedit dicta odio corrupti, nostrum eaque earum in voluptatibus perferendis rerum aspernatur explicabo delectus incidunt fugit.</p>
+        <PostButton />
+    </div>
+</template>
+
+<script setup>
+    import PostButton from './PostButton.vue';
+</script>
+
+<style scoped>
+    .post-card {
+        background-color: #f1f1f1;
+        border-radius: 10px;
+        padding: 20px;
+        margin: 10px 0;
+    }
+    h1 {
+        color: green;
+    }
+</style>
