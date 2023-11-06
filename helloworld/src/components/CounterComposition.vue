f8eb0099ab71dc717e88a9e582e07cd3dc514378
JuniorGodoyOrtega <godoyortegajuniorsilverio@gmail.com>
2023-11-05 23:51:10 -0300

    uso de setup en scripts

--- helloworld/src/components/CounterComposition.vue
+++ helloworld/src/components/CounterComposition.vue
@@ -2,16 +2,10 @@
   <h3>{{ counter }}</h3>
   <button @click="addCounter">Incrementar</button>
 </template>
-<script>
-import { defineComponent, ref } from "vue";
-export default defineComponent({
-  name: "CounterComposition",
-  setup() {
-    let counter = ref(0);
-    const addCounter = () => {
-      counter.value++;
-    };
-    return { counter, addCounter };
-  },
-});
+<script setup>
+import { ref } from "vue";
+let counter = ref(0);
+const addCounter = () => {
+  counter.value++;
+};
 </script>
