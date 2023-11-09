35aba9299135a4bae1ec18627b839be47ad4b680
JuniorGodoyOrtega <godoyortegajuniorsilverio@gmail.com>
2023-11-08 23:21:13 -0300

    bindeo a traves de eventos

--- helloworld/src/components/ClassBind.vue
+++ helloworld/src/components/ClassBind.vue
@@ -2,13 +2,22 @@
   <p
     v-bind:style="{
       backgroundColor: '#ccc',
-      color: '#fff',
+      color: c,
       padding: '10px',
       borderRadius: '5px',
     }"
   >
     El mensaje se ha enviado sastifactoriamente.
   </p>
+  <button @click="handleClick()">Cambia color</button>
 </template>
-<script setup></script>
+
+<script setup>
+import { ref } from "vue";
+let c = ref("#fff");
+const handleClick = () => {
+  c.value = "#ff0000";
+};
+</script>
+
 <style></style>
